0861ea65873a3cef6d991315bc5bd80560166091
Avto Chachanidze <83176152+Chachanidze29@users.noreply.github.com>
2022-07-07 11:05:40 +0400

    Delete Easy directory

--- Easy/lengthOfLastWord.js
+++
@@ -1,9 +0,0 @@
-/**
- * @param {string} s
- * @return {number}
- */
-let  lengthOfLastWord = function(s) {
-    let arr = s.split(' ');
-    arr = arr.filter(value => value !== '');
-    return arr[arr.length - 1].length;
-};
--- Easy/runningSum.js
+++
@@ -1,7 +0,0 @@
-let runningSum = function(nums) {
-    for(let i=1;i<nums.length;i++) {
-        nums[i] += nums[i-1];
-    }
-
-    return nums;
-};
